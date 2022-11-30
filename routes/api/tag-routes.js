a9c4c5474f0f0b200ef1a01fd43dbc1d29ad0465
OilPainter1 <hjcryns@gmail.com>
2022-11-30 16:07:10 -0600

    added put route for tag model

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -30,8 +30,14 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  const updatedTagName = await Tag.update({tag_name: req.body.tag_name}, {
+    where: {
+      id: req.params.id
+    }
+  })
+  res.json(updatedTagName)
 });
 
 router.delete('/:id', async (req, res) => {
